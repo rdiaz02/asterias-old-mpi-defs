72d60bb2285d6f8c873c9a631b993450ec98af47
www-data <www-data@karl02>
2007-03-23 12:51:42 +0100

    defs with different cpus

--- generate.defs.py
+++ generate.defs.py
@@ -5,7 +5,9 @@
 BASE_NAME = 'karl'
 BASE_IP = '192.168.7.'
 N_CPUS  = '4'
+N_CPUS4  = '4'
 N_CPUS2  = '2'
+N_CPUS1  = '1'
 
 for i in range(1, 32):
     if i < 10:
@@ -14,14 +16,21 @@ for i in range(1, 32):
 	j = str(i)
     outf = open('lamb-host.' + BASE_NAME + j + '.def', mode = 'w')
     outf2 = open('lamb-host.' + BASE_NAME + j + '.2cpu.def', mode = 'w')
+    outf1 = open('lamb-host.' + BASE_NAME + j + '.1cpu.def', mode = 'w')
+    outf4 = open('lamb-host.' + BASE_NAME + j + '.4cpu.def', mode = 'w')
+
     for m in range(i, 32):
     	if m != 21:  #and m != 27 and m != 31:
             outf.write(BASE_IP + str(m) + ' cpu=' + N_CPUS + '\n')
             outf2.write(BASE_IP + str(m) + ' cpu=' + N_CPUS2 + '\n') 
+            outf1.write(BASE_IP + str(m) + ' cpu=' + N_CPUS1 + '\n') 
+            outf4.write(BASE_IP + str(m) + ' cpu=' + N_CPUS4 + '\n') 
     for m in range(1, i):
     	if m != 21: # and m != 27 and m != 31:
             outf.write(BASE_IP + str(m) + ' cpu=' + N_CPUS + '\n')
             outf2.write(BASE_IP + str(m) + ' cpu=' + N_CPUS2 + '\n')             
+            outf1.write(BASE_IP + str(m) + ' cpu=' + N_CPUS1 + '\n')             
+            outf4.write(BASE_IP + str(m) + ' cpu=' + N_CPUS4 + '\n')             
     outf.close()
 
 
